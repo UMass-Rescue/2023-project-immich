0bb87cfd53e2adbf2df59f663b16ec0b22710478
George Bissias <gbiss@cs.umass.edu>
2024-01-18 09:19:30 -0500

    tweak electron window settings

--- electron/main.js
+++ electron/main.js
@@ -2,14 +2,22 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    minWidth: 1024,
+    minHeight: 768,
+    autoHideMenuBar: true,
   })
 
+  win.maximize()
   win.loadURL('http://localhost:2283')
 }
 
 app.whenReady().then(() => {
   createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
